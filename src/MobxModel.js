82645748b890e1d99a4edc1f8a666eda1efcf04e
Laszlo Kajan <laszlo.kajan@roche.com>
2018-03-05 08:57:42 +0100

    Call mobx.extendObservable() when adding new property to an observable object

--- src/MobxModel.js
+++ src/MobxModel.js
@@ -50,7 +50,15 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/Context'
 
         var node = iLastSlash === 0 ? this._observable : this._getNode(resolvedPath.substring(0, iLastSlash));
         if (node) {
-          node[property] = value;
+          // MobX will not react to observable properties that did not exist when tracking started.
+          // If any other observable causes the autorun to re-run, the autorun will start tracking the postDate as well.
+          if (!(property in node) && mobx.isObservableObject(node)) {
+          	var oExt = {};
+          	oExt[property] = value;
+          	mobx.extendObservable(node, oExt);
+          } else {
+            node[property] = value;
+          }
           return true;
         }
         return false;
@@ -78,7 +86,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/Context'
 
 		  // Check for out of bounds array index access to suppress warnings
 		  //	"[mobx.array] Attempt to read an array index (1) that is out of bounds (1). Please check length first. Out of bound indices will not be tracked by MobX"
-          if(!mobx.isObservableArray(currentNode) || parts[i] < currentNode.length ) {
+          if (!mobx.isObservableArray(currentNode) || parts[i] < currentNode.length) {
         	currentNode = currentNode[parts[i]];
           } else {
           	currentNode = null;
